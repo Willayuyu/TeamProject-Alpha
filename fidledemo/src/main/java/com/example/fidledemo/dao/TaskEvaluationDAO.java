163aee07a0b28a48469da01c3fde88566a68bfd3
fzu-zsp <934110724@qq.com>
2021-04-30 20:51:00 +0800

    增加部分DAO层代码，修改部分DAO层代码错误——221801124张思萍
    增加部分DAO层代码，修改部分DAO层代码错误——221801124张思萍

---
+++ fidledemo/src/main/java/com/example/fidledemo/dao/TaskEvaluationDAO.java
@@ -0,0 +1,45 @@
+package com.example.fidledemo.dao;
+
+import com.example.fidledemo.BO.EvaluationBO;
+import com.example.fidledemo.DO.TaskEvaluationDO;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: ZSP
+ */
+public interface TaskEvaluationDAO {
+
+    /**
+     * 插入任务委托评价
+     * @param taskEvaluation
+     */
+    void insertTaskEvaluation(TaskEvaluationDO taskEvaluation);
+
+    /**
+     * 删除
+     * @param id
+     */
+    void deleteTaskEvaluation(Long id);
+
+    /**
+     * 根据id查找
+     * @param id
+     * @return
+     */
+    EvaluationBO getTaskEvaluation(Long id);
+
+    /**
+     * 筛选
+     * @param taskEvaluation
+     * @return
+     */
+    List<EvaluationBO> listTaskEvaluation(TaskEvaluationDO taskEvaluation);
+
+    /**
+     * 更新
+     * @param taskEvaluation
+     */
+    void updateTaskEvaluation(TaskEvaluationDO taskEvaluation);
+}
