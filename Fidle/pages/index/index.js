04479f996eb6f912ca18068b4a71c733343ec769
fzu221801328 <962900904@qq.com>
2021-05-01 16:16:31 +0800

    完成首页活动页的列表项的编写，并且调整样式，与其他部分统一
    也修改了tabs的小错误

--- Fidle/pages/index/index.js
+++ Fidle/pages/index/index.js
@@ -1,18 +1,42 @@
-// pages/index2/index2.js
+// pages/index/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    secondhandList: [],
+    goodsPrice: "30",
+    goodsOriginPrice: "75",
+    goodsTitle: "软件工程 第八版 全新未拆封 好价速来！",
+    goodsImageURL: "/images/book.png",
+    goodsTagsList: [
+      "全新",
+      "教材",
+      "大三",
+      "软件工程"
+    ],
+    taskPrice: "30",
+    taskTitle: "找人拿快递 3小件 送到41号楼",
+    taskTagsList: [
+      "拿快递",
+      "顺丰"
+    ],
+    activityImageURL: "http://5b0988e595225.cdn.sohucs.com/images/20190311/ab66040c529445778cf31ced4ba24657.jpeg",
+    activityTitle: "百米画卷",
+    address: "青春广场",
+    date: "2021年4月23日",
+    oganizer: "2018级软件工程3班",
+    activityTagsList: [
+      "团立项",
+      "五四活动"
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //wx.setNavigationBarTitle({ title: '首页' });  
   },
 
   /**
@@ -56,7 +80,6 @@ Page({
   onReachBottom: function () {
 
   },
-  
 
   /**
    * 用户点击右上角分享
@@ -64,6 +87,7 @@ Page({
   onShareAppMessage: function () {
 
   },
+
 
   /**
    * 获取二手物品列表
@@ -83,6 +107,4 @@ Page({
     //   }
     // })
   }
-
-
 })
