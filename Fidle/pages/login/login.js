1d98e5946396ef72535cf44a58f7628ff5661680
fzu221801328 <962900904@qq.com>
2021-05-10 20:50:23 +0800

    添加登录功能的部分代码
    添加登录功能的部分代码

--- Fidle/pages/login/login.js
+++ Fidle/pages/login/login.js
@@ -8,6 +8,8 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUseGetUserProfile: false,
+    user: {},
+    token: "",
   },
 
   /**
@@ -66,11 +68,105 @@ Page({
 
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+    console.log("hasuserinfo="+this.hasUserInfo)
+    let that = this;
+    //1 wx.login获取code
+    wx.login({
+      success (res) {
+        if (res.code) {
+          console.log('res.code='+res.code)
+          //2 发起网络请求,获取token和用户信息
+          wx.request({
+            url: 'http://47.106.241.182:8080/login/loginRequest',
+            method: 'POST',
+            data:{code: res.code},
+            header: {
+              'content-type': 'application/x-www-form-urlencoded',
+            },
+            success: (res) => {
+              console.log(res)
+              // 把数据存下来
+              that.setData({
+                user: res.data.user,
+                token: res.data.token,
+              }) 
+            },
+            fail:function(err){
+              console.log('fail')
+              console.log(err)
+            },
+            //先注释掉第二个请求
+            /*
+            complete: function () { 
+              //不论成功还是失败都会到这里面。第二个请求放在第一个请求的complete里面                         
+              try {
+                console.log('that.user.nickname='+that.user.nickname)
+                //3 判断是否用户信息中的nickname和portrait为空
+                if(that.user.nickname=="" && that.user.portrait=="") {
+                  //3.1 为空，则用户为第一次登录
+                  //必须留在当前页面，进行授权
+                  if (wx.getUserProfile) {
+                    that.setData({
+                      canIUseGetUserProfile: true
+                    })
+                  }
+                  //4 授权成功后，发送用户信息给后端，返回完整的用户信息
+                  wx.request({
+                    url: 'http://xx/login/userAuth', 
+                    data: {
+                      nickname: user.nickname,
+                      avatar_url: user.portrait,
+                    },
+                    header: {
+                      'token': token,
+                    },
+                    success (res) {
+                      console.log(res.data)
+                      //把数据存下来
+                      that.setData({
+                        user: res.data.user,
+                        token: res.data.token,
+                      }) 
+                    },
+                    fail:function(err){
+                      console.log(err)
+                    },
+                  })
+
+                }
+                else {
+                  
+                }       
+              } catch (e) { }
+            }*/
+           })
+
+          //3.2 昵称和头像不为空
+          //直接登录，跳转至首页
+          //5 授权成功后，也获取了昵称和头像，跳转至首页
+          //把token和user存入Storage
+          /*if(that.user.nickname=="" && that.user.portrait=="") {
+            wx.setStorageSync('token', that.token)
+            wx.setStorageSync('id', that.user.id)
+            wx.setStorageSync('portrait', that.user.portrait)
+            wx.setStorageSync('qq', that.user.qq)
+            wx.setStorageSync('username', that.user.username)
+            wx.setStorageSync('creditScore', that.user.credit.creditScore)
+            wx.setStorageSync('dislikeNum', that.user.credit.dislikeNum)
+            wx.setStorageSync('likeNum', that.user.credit.likeNum)
+
+            wx.redirectTo({
+              url: '/pages/index/index'
+            })
+          }*/
+
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+
+  
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
