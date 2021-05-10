8dd5e7523174441ec7516a849204d6fda387e0f1
BobbyShao <79575907+BobbyShao@users.noreply.github.com>
2021-05-10 21:59:44 +0800

    添加获取登陆用户信息和修改信息的交互
    用户在成功登录后向后端发送请求获取用户信息，在修改个人信息后向后端更新

--- Fidle/app.js
+++ Fidle/app.js
@@ -5,11 +5,31 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    console.log("onlaunch")
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log("登陆成功");
+        // var that=this;
+        // wx.request({
+        //   url: 'http://localhost:8080/personalPage/getHomePageById/id',
+        //   method: "GET",
+        //   header: {
+        //     'content-type':'application/json'
+        //   },
+        //   success: function (res){
+        //     //登录时获取用户信息赋给全局数据
+        //     that.globalData.userid=res.data.id;
+        //     that.globalData.username=res.data.username;
+        //     that.globalData.credit=res.data.credit.credit_score;
+        //     that.globalData.goodcmt=res.data.like_num;
+        //     that.globalData.badcmt=res.data.dislike_num;
+        //     that.globalData.phonenum=res.data.tel;
+        //     that.globalData.qqnum=res.data.qq;
+        //   }
+        // })
       }
     })
   },
