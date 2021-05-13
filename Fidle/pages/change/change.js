de304acdab08b2be7dbe88ffa567d0a82c041900
BobbyShao <79575907+BobbyShao@users.noreply.github.com>
2021-05-13 22:11:12 +0800

    change.js覆盖错了

--- Fidle/pages/change/change.js
+++ Fidle/pages/change/change.js
@@ -4,70 +4,84 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {//没有真实数据的时候先填充一下
-      imgsrc:app.globalData.imgsrc,
+  data: {
+      imgsrc:app.globalData.user.portrait,
       username:app.globalData.username,
       phonenum:app.globalData.phonenum,
       qqnum:app.globalData.qqnum,
   },
 
-  change: function() {
-    wx.navigateTo({
-      url: '/pages/change/change',
-    })
-  },
+  change: function(e) {
+    var username=e.detail.value.username.trim();
+    var phonenum=e.detail.value.phonenum.trim();
+    var qqnum=e.detail.value.qqnum.trim();
+    
+    if(username.length==0||phonenum.length==0||qqnum.length==0) {
+      wx.showToast({
+        title: '信息不能为空！',
+        duration: 1000,
+        icon: 'none'
+      })
+    }
+     else if(phonenum.length!=11||!(/^1[3|4|5|7|8][0-9]{9}$/.test(phonenum))) {
+      wx.showToast({
+        title: '电话号码输入有误！',
+        duration: 1000,
+        icon: 'none'
+      })
+    }
+    else{
+      //将修改的信息赋给全局数据 方便调用
+      app.globalData.user.username=username;
+      app.globalData.phonenum=phonenum;
+      app.globalData.qqnum=qqnum;
+      console.log("填入的用户名："+username);
+      console.log("填入的电话号码："+phonenum);
+      console.log("填入的qq："+qqnum);
 
-  logout: function() {
-    wx.showModal({
-      content: '确认退出当前账号吗？',
-      success: function (res) {
-        //进行退出登录操作：把Storage中的flag置为false
-        wx.setStorageSync('isLogin',false)
-        if (res.confirm) {
-          wx.navigateTo({
-            url: '/pages/login/login',
+      wx.request({
+        url: 'http://120.77.210.142:8080/personalPage/alterInformation',
+        data: {
+          //将修改的数据传给后台进行同步修改
+          username: username,
+          tel: phonenum,
+          qq: qqnum
+        },
+        method: 'POST',
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          'Cookie': wx.getStorageSync('sessionid')
+        },
+        success: function(res){
+          console.log(res);
+          wx.showToast({
+            title: '修改成功！',
+            icon: 'success',
+            duration: 1000,
+            success:function(){ 
+              setTimeout(function () { 
+                  wx.redirectTo({ 
+                      url: '/pages/changeInf/changeInf'
+                   }) 
+               }, 1000) 
+            } 
           })
-          console.log('用户点击确定');
-        } else if (res.cancel) {
-          console.log('用户点击取消');
+        },
+        fail: function(res){
+          console.log("修改失败！")
         }
-      }
-    })
+      })      
+    }   
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.setNavigationBarTitle({ title: '个人信息' });
+    wx.setNavigationBarTitle({ title: '修改个人信息' }) ;
     this.setData({
       imgsrc:app.globalData.user.portrait,
-      username:app.globalData.user.username,
-    });
-    let that=this;
-    wx.request({
-      url: 'http://120.77.210.142:8080/personalPage/getHomePageById/'+app.globalData.user.id,
-      method: 'GET',
-      header: {
-        "Content-Type": "application/json",
-        'Cookie': wx.getStorageSync('sessionid')
-      },
-      success: function(res){
-        console.log(res);
-        that.setData({
-          //onload装载页面要显示的信息
-          imgsrc:res.data.data.portrait,
-          username:res.data.data.username,
-          phonenum:res.data.data.tel,
-          qqnum:res.data.data.qq
-        })
-        app.globalData.phonenum=res.data.data.tel;
-        app.globalData.qqnum=res.data.data.qq;
-      },
-      fail: function(res){
-        console.log("根据id获取个人信息失败")
-      }
-    })    
+    })
   },
 
   /**
@@ -81,11 +95,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.setData({
-      username:app.globalData.user.username,
-      phonenum:app.globalData.phonenum,
-      qqnum:app.globalData.qqnum,
-    })
+
   },
 
   /**
@@ -99,9 +109,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    wx.reLaunch({
-      url: '/pages/myInf/myInf'
-    })
+
   },
 
   /**
