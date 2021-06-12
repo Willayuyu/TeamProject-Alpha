b319e34acb574cb868d59d81ea943355d7656e57
fzu221801328 <962900904@qq.com>
2021-06-12 16:16:39 +0800

    修改侧边栏的图标，添加侧边栏点击事件
    修改侧边栏的图标，添加侧边栏点击事件

---
+++ fffvue/src/components/Sidebar.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <!-- el-menu开启vue-router模式，菜单项的index属性作为路径path使用 -->
+  <el-menu
+    :router="true"
+    default-active="2"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+    <el-menu-item index="/">
+      <i class="iconfont icon-home icon"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+    <el-menu-item index="/goods">
+      <i class="iconfont icon-Shoppingcartfinancebusiness icon"></i>
+      <span slot="title">二手物品</span>
+    </el-menu-item>
+    <el-menu-item index="/task">
+      <i class="iconfont icon-shoppingbag icon"></i>
+      <span slot="title">任务委托</span>
+    </el-menu-item>
+    <el-menu-item index="/activity">
+      <i class="iconfont icon-horn icon"></i>
+      <span slot="title">活动信息</span>
+    </el-menu-item>
+  </el-menu>
+
+</div>
+</template>
+<script>
+export default{
+     methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+}
+</script>
+<style>
+.icon {
+  margin-right: 15px;
+  margin-left: 15px;
+}
+</style>
--- fffvue/src/main.js
+++ fffvue/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 import '/dist/fonts/iconfont.css'
-
+import './assets/fonts/download/font_2608416_q83rkcif4ap/iconfont.css'
 
 Vue.config.productionTip = false
 
