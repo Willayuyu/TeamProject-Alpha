21e556c013efe0313bb048eb69c9f51df1ee6c2b
fzu221801328 <962900904@qq.com>
2021-06-15 21:08:58 +0800

    添加导航条的跳转功能，和侧边栏的默认选择，美化导航条和侧边栏

--- fffvue/src/components/Sidebar.vue
+++ fffvue/src/components/Sidebar.vue
@@ -3,13 +3,14 @@
   <!-- el-menu开启vue-router模式，菜单项的index属性作为路径path使用 -->
   <el-menu
     :router="true"
-    default-active="2"
+    default-active="/goodsList"
     class="el-menu-vertical-demo"
     @open="handleOpen"
     @close="handleClose"
     background-color="#545c64"
     text-color="#fff"
-    active-text-color="#ffd04b">
+    active-text-color="#7db5ee"
+    >
     <el-menu-item index="/">
       <i class="iconfont icon-home icon"></i>
       <span slot="title">首页</span>
