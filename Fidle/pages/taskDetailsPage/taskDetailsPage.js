b78504e154e565c6f88b0d4a48dbefde394c8e15
Willayuyu <51328670+Willayuyu@users.noreply.github.com>
2021-06-15 21:56:20 +0800

    修改详情页URL
    修改详情页URL

--- Fidle/pages/taskDetailsPage/taskDetailsPage.js
+++ Fidle/pages/taskDetailsPage/taskDetailsPage.js
@@ -27,7 +27,7 @@ Page({
   getTaskDetail: function () {
     let that = this;
     wx.request({
-      url: 'http://47.106.241.182:8080/task/getTaskDetailById/' + that.data.id,
+      url: 'http://47.106.241.182:8082/task/getTaskDetailById/' + that.data.id,
       method: "GET",
       header: {
         'content-type': 'application/json',
@@ -71,7 +71,7 @@ Page({
     let that = this;
     if (that.data.collectState == -1) {
       wx.request({
-        url: 'http://47.106.241.182:8080/task/collectTask/' + that.data.id,
+        url: 'http://47.106.241.182:8082/task/collectTask/' + that.data.id,
         method: 'GET',
         header: {
           'content-type': 'application/json',
@@ -96,7 +96,7 @@ Page({
       })
     } else if (that.data.collectState == 1) {
       wx.request({
-        url: 'http://47.106.241.182:8080/task/cancelCollectTask/' + that.data.id,
+        url: 'http://47.106.241.182:8082/task/cancelCollectTask/' + that.data.id,
         method: 'GET',
         header: {
           'content-type': 'application/json',
@@ -154,7 +154,7 @@ Page({
     var reason = that.data.inputValue;
     console.log(reason);
       wx.request({
-        url: 'http://120.77.210.142:8080/goods/reportGoods',
+        url: 'http://120.77.210.142:8082/goods/reportGoods',
         method: 'POST',
         header: {
           'Content-Type': 'application/x-www-form-urlencoded',
